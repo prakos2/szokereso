75f72763ffd67f930dbb53c935f97314da7cbcc5
prakos2 <preil.akos.levente.2020i@bankitatabanya.hu>
2022-05-31 20:53:17 +0200

    Játékvezérlő, main újítások

--- src/Jatekvezerlo.py
+++ src/Jatekvezerlo.py
@@ -1,2 +1,46 @@
 class Jatekvezerlo():
-    pass
+    def __init__(self):
+        # Játékállás
+        self.JATEKALLASOK = ["menu", "jatek", "vegeredmeny"] # Sorrendben lévő játékállások
+        self.jatekallas = self.JATEKALLASOK[0]               # Első lehetséges játékállás megadása
+        self.jatek_adatok={
+            "szint":0,              # jelenlegi szint
+            "szavak":0,             # össz szó
+            "megtalalt_szavak":0,   # megtalált szavak / szint
+            "ossz_szavak":0,        # összes megtalált szó
+            "eltelt_jatekido":0,    # ms
+            "ossz_jatekido":0       # ms
+        }
+        self.szavak = []
+
+    def f_jatekallas(self, in_jatekallas):
+        """
+        Játékállás frissítése
+        """
+        if in_jatekallas in self.JATEKALLASOK:
+            self.jatekallas = in_jatekallas
+        else:
+            raise Exception("[Jatekvezerlo] Megadott játékállás helytelen.")
+
+    def reset(self):
+        """
+        A játék újraindítása
+        """
+        try:
+            self.f_jatekallas("menu")
+            for i in self.jatek_adatok:
+                self.jatek_adatok[i] = 0
+        except:
+            raise Exception("[Jatekvezerlo] Reset nem lehetséges.")
+    
+    def szo_racs(self):
+        """
+        Megadott szavakból többdimenziós tömböt generál
+        """
+        pass
+
+    def uj_szint(self):
+        """
+        szint, megtalalt_szavak nullázása, új rács generálása
+        """
+        pass
--- src/main.py
+++ src/main.py
@@ -1,28 +1,48 @@
 import pygame as pg
+from Jatekvezerlo import Jatekvezerlo
 
 class Szokereso():
     """
     Inicializálás
     """
     def __init__(self):
+        # PyGame
         pg.init()
-        self.FELBONTAS=self.SZELESSEG, self.MAGASSAG=(pg.display.Info().current_w, pg.display.Info().current_h) # A képernyő teljes mérete.
-        self.ABLAK_MERET=(self.SZELESSEG/1.5, self.MAGASSAG/1.5)
-        self.FPS=60
-        self.KEPERNYO=pg.display.set_mode(self.ABLAK_MERET)
-        self.ORA = pg.time.Clock()
-        pg.display.set_caption("Szókereső")
+        self.ORA = pg.time.Clock() # óra
+        # PyGame ablak
+        self.FELBONTAS=self.SZELESSEG, self.MAGASSAG=(pg.display.Info().current_w, pg.display.Info().current_h) # A képernyő teljes mérete
+        self.ABLAK_MERET=(self.SZELESSEG/1.5, self.MAGASSAG/1.5) # Relatív ablakméret
+        self.FPS=60 # Képkocka/s
+        self.KEPERNYO=pg.display.set_mode(self.ABLAK_MERET) # képernyőméret
+        pg.display.set_caption("Szókereső") # ablaknév
+        # Játékvezérlő
+        self.JATEKVEZERLO = Jatekvezerlo()
+        self.JATEKVEZERLO.reset()
     
     """
     Tickre változó függvény
     """
     def _folyamat(self):
         while True:
+            # Kilépés kezelése
             for i in pg.event.get():
                 if i.type == pg.QUIT:
                     return
+            # Képfrissítés
             pg.display.flip()
             self.ORA.tick(self.FPS)
+            # Játékvezérlés
+            self.JATEKVEZERLO.frissit()
+            if self.JATEKVEZERLO.jatekallas == "menu":
+                self.KEPERNYO.fill((255,255,255))
+                self.JATEKVEZERLO.f_jatekallas("jatek")
+            elif self.JATEKVEZERLO.jatekallas == "jatek":
+                self.KEPERNYO.fill((125,52,52))
+                self.JATEKVEZERLO.f_jatekallas("vegeredmeny")
+            elif self.JATEKVEZERLO.jatekallas == "vegeredmeny":
+                self.KEPERNYO.fill((52,32,12))
+                self.JATEKVEZERLO.f_jatekallas("menu")
+
 
 if __name__ == "__main__":
     jatek_peldany = Szokereso()
