287c0a4d2f84f367523bb2b4561c2347f271d9fe
prakos2 <preil.akos.levente.2020i@bankitatabanya.hu>
2022-05-31 18:33:57 +0200

    Jegyzet hozzáadása

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-import pygame
+import pygame as pg
+
+class Szokereso():
+    """
+    
+    """
+    def __init__(self):
+        pg.init()
+    
+    def jatek_folyamat():
+        while True:
+            pass
+
+if __name__ == "__main__":
+    Szokereso.jatek_folyamat()
