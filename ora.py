08c0dd6915c101db8335ab586451fc6ee670680b
Zsolt-Csuhaj <77265795+Zsolt-Csuhaj@users.noreply.github.com>
2022-06-02 19:01:35 +0200

    Add files via upload

---
+++ ora.py
@@ -0,0 +1,33 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode((500,500))
+
+done= False
+secs=00
+mins=00
+hours=00
+
+font=pygame.font.Font('freesansbold.ttf',32)
+text=font.render("{}:{}:{}".format(hours,mins,secs), True,(255,255,255), (00,00,00))
+textRect=text.get_rect()
+textRect.center=500//2,500//2
+
+clock=pygame.time.Clock()
+
+while not done:
+    clock.tick(2)  
+    secs +=1
+    window.blit(text,textRect)
+    if secs==60:
+        secs=0
+        mins+=1
+    if   mins==60:
+        mins==0
+        secs==0
+        hours+=1
+    text=font.render("{}:{}:{}".format(hours,mins,secs),True,(255,255,255),(00,00,00))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            quit()
+    pygame.display.update()
