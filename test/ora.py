0ac26d8893d8ac9d65fb9b75a6b4f79863e7c9c3
KAdam050728 <101458875+KAdam050728@users.noreply.github.com>
2022-06-03 15:24:30 +0200

    ora befejezese
    beallithato perc es masodperc, amely lefele csokken

--- test/ora.py
+++ test/ora.py
@@ -13,20 +13,28 @@ perc_szoveg=str(perc_szamlalo)
 
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 
-font=pygame.font.SysFont('Consolas', 30)
+BETUTIPUS=pygame.font.SysFont('Consolas', 30)
 
 while True:
-    for i in pygame.event.get():
-        if i.type==pygame.USEREVENT: 
-            masodperc_szamlalo-=1
-            if masodperc_szamlalo>0:
-                masodperc_szoveg=str(masodperc_szamlalo)
-            else:
-                break
-    else:
-        ablak.fill((0,0,0))
-        ablak.blit(font.render(masodperc_szoveg,perc_szoveg, True, (255,255,255)), (32, 48))
-        pygame.display.flip()
-        ora.tick(60)
-        continue
+    if masodperc_szamlalo!=0:
+        for i in pygame.event.get():
+            if i.type==pygame.USEREVENT: 
+                masodperc_szamlalo-=1
+                if masodperc_szamlalo>0:
+                    masodperc_szoveg=str(masodperc_szamlalo)
+                else:
+                    if perc_szamlalo!=0:
+                        perc_szamlalo-=1
+                        masodperc_szamlalo=60
+                        if perc_szamlalo==0:
+                            break
+                    else:
+                        break
+        else:
+            ablak.fill((0,0,0))
+            ablak.blit(BETUTIPUS.render(f"{perc_szoveg}:{masodperc_szoveg}", True, (255,255,255)), (32, 48))
+            pygame.display.flip()
+            ora.tick(60)
+            continue
+        break
     break
