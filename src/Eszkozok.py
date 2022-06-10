c349b7548440b9ba008318c62a760446894ef668
KAdam050728 <101458875+KAdam050728@users.noreply.github.com>
2022-06-10 18:34:38 +0200

    szavak elhelyezése

--- src/Eszkozok.py
+++ src/Eszkozok.py
@@ -4,27 +4,23 @@ def idoformat(t):
     return str(t // 60) + ":" + str(t % 60).zfill(2)
 
 def szokeres(szavak,n):
-    tomb=[]
-    for i in range(len(szavak)):
-        if len(szavak[i])<=n:
-            tomb.append(szavak[i])
-    for i in range(len(tomb)):
-        return tomb
-
+    szuro = [x for x in range(len(szavak)) if len(szavak[x]) <= n]
+    if len(szuro) > 0:
+        return random.randint(0,len(szuro)-1)
+    else:
+        return False
+    
 def szo_elhelyezes(szavak,n):
-    szo_grid = [[0 for x in range(n)] for j in range(n)]
+    racs = [[0 for x in range(n)] for j in range(n)]
     for i in szavak:
-        koordinata=(random.randint(0,n-1),random.randint(0,n-1))#allista, elem
-        szo_grid[koordinata[0]][koordinata[1]]="M"
-        for j in szo_grid:
+        random_koord = (random.randint(0,len(racs)-1), random.randint(0,len(racs)-1))#allista, pozicio
+        szo = szokeres(szavak, n-random_koord[1])
+    if szo != False:
+        for j in range(n-(random_koord[1]-1), n+1):
             print(j)
-        print(n-koordinata[1])
-        if len(i)<=n-koordinata[1]:
-            print("megfelel")
-        else:
-            print("nem felel meg")
+            racs[random_koord[0]][j] = szavak[szo][j]
+        print("OK")
+    print(racs)
 
-szavak=["aaa","bbb","aaaa","bbbb","aaaaa","bbbbb","aaaaaa","bbbbbb","aaaaaaa","bbbbbbb"]
-n=4
-tomb=szokeres(szavak,n)
-szo_elhelyezes(tomb,n)
+
+szo_elhelyezes(["aaa","bbb","aaaa","bbbb","aaaaa","bbbbb","aaaaaa","bbbbbb","aaaaaaa","bbbbbbb"],4)
